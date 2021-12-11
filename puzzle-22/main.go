9f5f6dafa14e95c7415d2c3d6a7699d6bbc29122
Markus Radziej <markus.radziej@bayer.com>
2021-12-11 11:15:08 +0100

    feat: add solutions to both puzzles of day 11

---
+++ puzzle-22/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+var adjacency = [...]int{-1, 0, 1}
+
+func main() {
+	workingDirectory, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var energies [10][10]int
+	row := 0
+	for line := range readLines(workingDirectory + "/puzzle-22/input.txt") {
+		for column, character := range line {
+			number, err := strconv.Atoi(string(character))
+			if err != nil {
+				log.Fatal(err)
+			}
+			energies[row][column] = number
+		}
+		row++
+	}
+
+	for step := 1; ; step++ {
+		var flashed [10][10]bool
+		for row := range energies {
+			for column := range energies[row] {
+				raiseEnergy(&energies, &flashed, row, column)
+			}
+		}
+		sumFlashes := 0
+		for row := range flashed {
+			for column := range flashed[row] {
+				if flashed[row][column] {
+					sumFlashes++
+				}
+			}
+		}
+
+		if step%10 == 0 {
+			fmt.Printf("Number of flashes at %v steps: %v\n", step, sumFlashes)
+		}
+		if sumFlashes == 100 {
+			fmt.Printf("Fully synchronized at %v steps", step)
+			break
+		}
+	}
+}
+
+func readLines(p string) chan string {
+	channel := make(chan string, 1)
+
+	go func() {
+		defer close(channel)
+
+		file, err := os.Open(p)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			channel <- scanner.Text()
+		}
+	}()
+	return channel
+}
+
+func printState(energies [10][10]int) {
+	for _, row := range energies {
+		for _, field := range row {
+			fmt.Printf("%v", field)
+		}
+		fmt.Println("")
+	}
+}
+
+func raiseEnergy(energies *[10][10]int, flashed *[10][10]bool, row int, column int) {
+	// Octopi only flash once
+	if flashed[row][column] {
+		return
+	}
+	// Raise energy level
+	if energies[row][column] < 9 {
+		(*energies)[row][column]++
+	} else {
+		(*energies)[row][column] = 0
+		(*flashed)[row][column] = true
+		// Raise adjacent energy levels
+		for _, rowOffset := range adjacency {
+			for _, columnOffset := range adjacency {
+				if row+rowOffset >= 0 && row+rowOffset < 10 && column+columnOffset >= 0 && column+columnOffset < 10 {
+					raiseEnergy(energies, flashed, row+rowOffset, column+columnOffset)
+				}
+			}
+		}
+	}
+}
